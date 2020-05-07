b1c8b9b8607142695352c2910c7e8714dafa3ec0
Mohamadelattar <mohamadelattar99@gmail.com>
2020-05-07 22:52:23 +0000

    Fixer L'interface Modifier ou supprimer un contrat

--- src/application/GestionContrat.java
+++ src/application/GestionContrat.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -19,6 +20,9 @@ public class GestionContrat {
 
 	@FXML
 	private TextField codeContrat;
+	
+	@FXML
+	private TextField rechercher;
 	
 	@FXML
 	private DatePicker dateContrat;
@@ -100,6 +104,88 @@ public class GestionContrat {
 		}
 	}
 	
+	public void interfaceModifierUtilisateur(ActionEvent e) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource(("GUI/ModifierContrat.fxml")));
+		root.setOnMousePressed(Main.handlerPressed);
+		root.setOnMouseDragged(Main.handlerDragged);
+		Scene scene = new Scene(root);
+		Main.stage.setScene(scene);
+		Main.stage.centerOnScreen();
+	}
+	
+	public void rechercherContrat(ActionEvent e) throws SQLException {
+		String rCONTRAT = rechercher.getText();
+		String sql = "SELECT * FROM `contrat` WHERE codeContrat='"+rCONTRAT+"';";
+		Connection C = Login.connectDB();
+		PreparedStatement ps = C.prepareStatement(sql);
+		ResultSet result = ps.executeQuery();
+		if(result.next()) {
+			codeContrat.setText(result.getString(1));
+			Date sqlDateContrat=result.getDate(2);
+			dateContrat.setValue(sqlDateContrat.toLocalDate());
+			Date sqlDateEcheance = result.getDate(3);
+			dateEcheance.setValue(sqlDateEcheance.toLocalDate());
+		}
+		else {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Erreur");
+			alert.setHeaderText("Il n'y a aucun utilisateur avec ce CIN");
+			alert.show();			
+		}
+		C.close();
+	}
+	
+	public void modifierContrat(ActionEvent e) throws IOException, SQLException {
+		if(!codeContrat.getText().equals("") && !dateContrat.equals(null) && !dateEcheance.equals(null)) {
+			
+			Contrat contrat = new Contrat(codeContrat.getText(),dateContrat.getValue(),dateEcheance.getValue());
+			String sql = "SELECT `codeContrat` FROM `contrat` WHERE codeContrat='"+contrat.getCodeContrat()+"';";
+			Connection C = Login.connectDB();
+			PreparedStatement ps = C.prepareStatement(sql);
+			ResultSet result = ps.executeQuery();
+			if(result.next() && !contrat.getCodeContrat().equalsIgnoreCase(rechercher.getText())) {	
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("Erreur");
+				alert.setHeaderText("Il y'a déjà un utilisateur avec le même CIN");
+				alert.show();
+			}
+			else {
+				String sql2 = "UPDATE `contrat` SET `codeContrat`=?, `dateContrat`=?, `dateEcheance`=?  WHERE codeContrat=?;";
+				Connection C2 = Login.connectDB();
+				PreparedStatement ps2 = C2.prepareStatement(sql2);
+				ps2.setString(1, contrat.getCodeContrat());
+				ps2.setObject(2, contrat.getDateContrat());
+				ps2.setObject(3, contrat.getDateEcheance());
+				ps2.setString(4, rechercher.getText());
+				ps2.executeUpdate();
+				retourGestion();
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Succès");
+				alert.setHeaderText("Les données ont été enregistrées");
+				alert.show();
+			}
+		}
+		else {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Erreur");
+			alert.setHeaderText("Veuillez remplir tous les champs");
+			alert.show();
+		}
+	}
+	
+	public void supprimerContrat(ActionEvent e) throws SQLException, IOException {
+		String sql = "DELETE FROM `contrat` WHERE codeContrat=?;";
+		Connection C = Login.connectDB();
+		PreparedStatement ps = C.prepareStatement(sql);
+		ps.setString(1, rechercher.getText());
+		ps.executeUpdate();
+		C.close();
+		retourGestion();
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Succès");
+		alert.setHeaderText("L'utilisateur a été supprimé");
+		alert.show();
+	}
 	public void retourGestion() throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource(("GUI/GestionContrat.fxml")));
 		root.setOnMousePressed(Main.handlerPressed);
